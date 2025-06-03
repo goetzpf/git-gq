d73b087aaaa134b09fc6e61e9d6d1256b373687e
Goetz Pfeiffer <goetzpf@googlemail.com>
2025-06-03 18:39:23 +0200

    Add a test to ensure that a regexp only matches a single revision.

--- test/git_gq_dump.sh
+++ test/git_gq_dump.sh
@@ -8,7 +8,7 @@ GIT_GQ="$1"
 
 source util.sh
 
-SRCDIR="tmp_git_gq_pop"
+SRCDIR="tmp_git_gq_new"
 TMPDIR="tmp_git_gq_dump"
 
 cp -a $SRCDIR $TMPDIR
@@ -16,11 +16,15 @@ cp -a $SRCDIR $TMPDIR
 cd $TMPDIR || exit 1
 
 echo "# Dump an applied patch."
-echo "\$ git gq dump 'README was'"
-$GIT_GQ dump 'README was' | filter_commit_hash | filter_author | filter_mail_date | filter_git_index_hash
+echo "\$ git gq dump 'README'"
+$GIT_GQ dump 'README' 2>&1 | filter_commit_hash | filter_author | filter_mail_date | filter_git_index_hash
+
+echo
+echo "\$ git gq pop -a"
+$GIT_GQ pop -a | filter_git_head_hash
 
 echo
 echo "# Dump an unapplied patch."
-echo "\$ git gq dump 'script.sh-was'"
-$GIT_GQ dump 'script.sh-was' | filter_from_hash | filter_mail_from | filter_mail_date
+echo "\$ git gq dump 'README'"
+$GIT_GQ dump 'README' 2>&1 | filter_from_hash | filter_mail_from | filter_mail_date
 
