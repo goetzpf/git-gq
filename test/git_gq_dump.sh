17559301d5e12978a3be73aaa51994fe8f41c4b3
Goetz Pfeiffer <goetzpf@googlemail.com>
2025-06-07 17:01:05 +0200

    'git gq dump' now also allows to use revisions as specification.
    Now you can use for example:
    
      git gq dump HEAD

--- test/git_gq_dump.sh
+++ test/git_gq_dump.sh
@@ -28,3 +28,7 @@ echo "# Dump an unapplied patch."
 echo "\$ git gq dump 'README'"
 $GIT_GQ dump 'README' 2>&1 | filter_from_hash | filter_mail_from | filter_mail_date
 
+echo
+echo "# Dump an HEAD commit."
+echo "\$ git gq dump 'HEAD'"
+$GIT_GQ dump 'HEAD' 2>&1 | filter_commit_hash | filter_author | filter_mail_date | filter_git_index_hash
