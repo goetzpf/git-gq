267f4349672204e5efe234ffbc550dd14d9dcd86
Goetz Pfeiffer <goetzpf@googlemail.com>
2025-07-02 11:20:39 +0200

    The version was changed to 1.2.

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@
 project = 'git-gq'
 copyright = '2025, Goetz Pfeiffer <goetzpf@googlemail.com>'
 author = 'Goetz Pfeiffer <goetzpf@googlemail.com>'
-version = '1.1' #VERSION#
+version = '1.2' #VERSION#
 release = version
 
 # -- General configuration ---------------------------------------------------
